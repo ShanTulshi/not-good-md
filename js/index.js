f66920b2cd20461ed98e5c9df182a3484010b8c1
Shan Tulshi <shan.tulshi@gmail.com>
2018-10-01 02:06:35 -0500

    Separated files, not great but it'll do

---
+++ js/index.js
@@ -0,0 +1,68 @@
+window.onload = () => {
+	const compression_mode = 9;
+	const showdownOpts = {
+		tasklists: true,
+	};
+	const pakoOpts = {
+		level: compression_mode,
+	};
+	v = new Vue({
+		el: "#input",
+		data: {
+			input: "",
+		},
+		methods: {
+			renderMd: () => {
+				document.getElementById("output").innerHTML = new showdown.Converter(showdownOpts).makeHtml(this.input.value);
+				// Update query parameter
+				let url = new URL(window.location.href);
+				let params = url.searchParams;
+				// let df = pako.deflate(v.input, pakoOpts);
+				// console.log(df);
+				// df = btoa(df);
+				let df = v.input;
+				params.set("c", encodeURIComponent(df));
+				history.pushState(null, "", "?" + params.toString());
+			}
+		},
+		watch: {
+			input: function() {
+				Vue.nextTick(this.renderMd);
+			}
+		}
+	});
+
+	// Hide/Show button
+	function toggleEditor(val) {
+		let x = document.getElementById("input");
+		if(val === true || x.style.display === "none") {
+			x.style.display = "block";
+			document.getElementById("hide").innerHTML = "Hide Editor";
+		} else {
+			x.style.display = "none";
+			document.getElementById("hide").innerHTML = "Show Editor";
+		}
+	}
+	document.getElementById("hide").onclick = toggleEditor;
+
+	// Clear button
+	document.getElementById("clear").onclick = function() {
+		Cookies.remove("content");
+		v.input = "";
+		v.renderMd();
+	};
+
+	// Grab content from URL
+	let url = new URL(window.location.href);
+	let content = url.searchParams.get("c");
+	if(content) {
+		let df = decodeURIComponent(content);
+		// df = atob(df)
+		console.log(df);
+		v.input = df;
+		// v.input = pako.inflate(df, pakoOpts);
+		toggleEditor(false);
+	} else {
+		v.input = "# Your Markdown goes here";
+	}
+}
